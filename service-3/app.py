3541b7e8e27bacff27bce6fe54189fd496daaecd
BMustafa97 <bilalmustafa1797@gmail.com>
2021-02-23 22:24:22 +0000

    App Complete

--- service-3/app.py
+++ service-3/app.py
@@ -6,7 +6,7 @@ from os import getenv
 app = Flask(__name__)
 @app.route('/slogan')
 def slogans():
-    slogan = ["We Give You Wings", "Finger-Lickin' Food", "Eat Fresh and Tasty!", "We're always on your mind", "The Best in Manchester", "Freshly Fried!", "The best you can find!"]
+    slogan = ["We Give You Wings!", "Finger-Lickin' Food.", "Eat Fresh and Tasty!", "We're always on your mind.", "The Best in Manchester.", "Freshly Fried!", "The best you can find!", "Eating Properly!", "Your Grub Hub!", "Farm to Fork!", "Licensed to grill.", "Meat. Fire. Good.", "First, we eat.", "You say magic. We say Mexican.", "Do the salsa!"]
     return Response(random.choice(slogan), mimetype="text")
 #in this service I create the second part of restaurant
 

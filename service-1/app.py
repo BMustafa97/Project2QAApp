485e58b5e1a2c3e7a6414ed989eb606f62b60617
BMustafa97 <bilalmustafa1797@gmail.com>
2021-02-25 10:40:00 +0000

    GCP+DB

--- service-1/app.py
+++ service-1/app.py
@@ -4,10 +4,13 @@ import requests
 from application import routes
 import os
 from os import getenv
+import mysql.connector
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///testdb.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost/flaskdb"
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -20,14 +23,15 @@ class Generator(db.Model):
 db.drop_all()
 db.create_all()
 
-@app.route("/", methods=['GET'])
+@app.route("/home", methods=['GET'])
 def home():
-    product = requests.get("http://localhost:5003/product")
+    product = requests.get("http://34.105.75.14:5003/product")
     result = product.text
     results = Generator(result=result)
     db.session.add(results)
     db.session.commit()
     products = Generator.query.all()
+ #   products.execute("SELECT * FROM Generator LIMIT 5 OFFSET 2")
   #  limit = Generator.query.order_by(Generator.id.desc()).limit(5)
     return render_template("home.html", product=result, products=products)
 

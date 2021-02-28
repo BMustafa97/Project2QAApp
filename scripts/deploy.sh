0374c0f638b5c8634e01ba7eaebe39dc9e5c9dfd
BMustafa97 <66776309+BMustafa97@users.noreply.github.com>
2021-02-28 15:41:03 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 ssh manager << EOF
-#scp /home/jason/SFIA_Project_Week_9/docker-compose.yaml /home/jenkins/
-export SECRET_KEY=${SECRET_KEY} 
-export DATABASE_URI=${DATABASE_URI} 
 docker stack deploy --compose-file docker-compose.yaml project2
 EOF
